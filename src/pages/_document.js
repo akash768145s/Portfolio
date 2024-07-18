17016691079be9da8b18629afd2c5b2609c57240
akash768145s <akash2210476@ssn.edu.in>
2024-07-18 16:14:48 +0530

    e

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -30,6 +30,10 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang='en-GB'>
+        <Head>
+          <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+          <meta name="title" content="Akash-Web Developer"/>
+        </Head>
         <body>
           <Main />
           <NextScript />
