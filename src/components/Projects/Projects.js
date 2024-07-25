b98d1b73639d5438bb545cfeb823f72bed6cf310
akash768145s <akash2210476@ssn.edu.in>
2024-07-25 22:25:18 +0530

    hi

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -13,44 +13,52 @@ import {
   UtilityList,
   Img,
 } from "./ProjectsStyles";
-import { Section, SectionTitle,SectionText,SectionDivider } from "../../styles/GlobalComponents";
+import {
+  Section,
+  SectionTitle,
+  SectionText,
+  SectionDivider,
+} from "../../styles/GlobalComponents";
 import { projects } from "../../constants/constants";
 
 const Projects = () => (
-  <><Section id="projects">
-    <SectionTitle main>Projects</SectionTitle>
-    <SectionText>
-      I have experience working with a variety of technologies as a developer.
-      I've developed and maintained multiple projects using these technologies,
-      and I'm always eager to learn more.
-    </SectionText>
-    <GridContainer>
-      {projects.map((p, i) => {
-        return (
-          <BlogCard key={i}>
-            <Img src={p.image} />
-            <HeaderThree title={p.title}>{p.title}</HeaderThree>
-            <Hr />
+  <>
+    <Section id="projects">
+      <SectionTitle main>Projects</SectionTitle>
+      <SectionText>
+        I've delivered impactful solutions through various client projects.
+        Explore some of my standout personal projects below.
+      </SectionText>
 
-            <CardInfo className="card-info">{p.description}</CardInfo>
-            <div>
-              <TitleContent>Tech Stack</TitleContent>
+      <GridContainer>
+        {projects.map((p, i) => {
+          return (
+            <BlogCard key={i}>
+              <Img src={p.image} />
+              <HeaderThree title={p.title}>{p.title}</HeaderThree>
               <Hr />
-              <TagList>
-                {p.tags.map((t, i) => {
-                  return <Tag key={i}>{t}</Tag>;
-                })}
-              </TagList>
-            </div>
-            <UtilityList>
-              <ExternalLinks href={p.visit}>Live Preview</ExternalLinks>
-              <ExternalLinks href={p.source}>Source Code</ExternalLinks>
-            </UtilityList>
-          </BlogCard>
-        );
-      })}
-    </GridContainer>
-  </Section><SectionDivider colorAlt /></>
+
+              <CardInfo className="card-info">{p.description}</CardInfo>
+              <div>
+                <TitleContent>Tech Stack</TitleContent>
+                <Hr />
+                <TagList>
+                  {p.tags.map((t, i) => {
+                    return <Tag key={i}>{t}</Tag>;
+                  })}
+                </TagList>
+              </div>
+              <UtilityList>
+                <ExternalLinks href={p.visit}>Live Preview</ExternalLinks>
+                <ExternalLinks href={p.source}>Source Code</ExternalLinks>
+              </UtilityList>
+            </BlogCard>
+          );
+        })}
+      </GridContainer>
+    </Section>
+    <SectionDivider colorAlt />
+  </>
 );
 
 export default Projects;
